d29eb4b5e9a6ad038362ddc7185022638dc05e04
Neuron7 <rk0004334@gmail.com>
2022-09-21 23:42:43 +0530

    third commit

--- index.js
+++ index.js
@@ -23,11 +23,13 @@ console.log(typeof NaN);
 
 console.log("Name");
 
+//closure = function combined together with its lexical scope is called closure
 function x(){
     var a = 10;
     function y(){
         console.log(a);
     }
+    a=195465;
     return y;
 }
 
